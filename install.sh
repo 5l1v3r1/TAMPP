eb64a819d99dd10462d709443247bc69f4c92c55
Abdul Muttaqin <fdciabdul@protonmail.com>
2021-02-23 01:16:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,4 +20,4 @@ pkg install php php-apache wget mariadb perl -y
 wget --progress=bar:force https://raw.githubusercontent.com/fdciabdul/TAMPP/main/httpd.conf && mv httpd.conf $PREFIX/etc/apache2
 mkdir /sdcard/www
 apachectl
-echo "TAMPP BERJALAN PADA PORT 8080 \n Silahkan buka browser kalian dan buka url localhost:8080\n Untuk directory root ada di folder www di memori internal kalian"
+echo -e "TAMPP BERJALAN PADA PORT 8080 \n Silahkan buka browser kalian dan buka url localhost:8080\n Untuk directory root ada di folder www di memori internal kalian"
